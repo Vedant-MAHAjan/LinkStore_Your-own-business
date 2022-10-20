8bed825b3ab35d4815db465fc771ca737f831e8c
Vedant Mahajan <mahajan@git>
2022-10-20 20:20:17 +0530

    email and api added

--- cart.js
+++ cart.js
@@ -134,6 +134,146 @@ console.log(clearCart);
 
 // ################## PAYMENT #############/
 
+// const payment = document.getElementById("payment");
+// var changeBody1 = document.getElementById("product1");
+// var changeBody2 = document.getElementById("product2");
+// var changeBody3 = document.getElementById("product3");
+// var changeBody4 = document.getElementById("product4");
+
+
+// // var payWall = document.getElementsByTagName("section");
+// //console.log(changeBody1);
+
+
+// (function() {
+//   payment.addEventListener("click", function() {
+    
+//   })
+// })();
+
+
+
+// (function() {
+//   payment.addEventListener("click", function() {
+//     if(cart_number == 0)
+//     {
+//       alert("Empty Cart");
+//     }
+//     else
+//     {
+      
+//       changeBody1.innerHTML = `
+//     <!DOCTYPE html>
+//     <html lang="en">
+//     <head>
+//         <meta charset="UTF-8">
+//         <meta http-equiv="X-UA-Compatible" content="IE=edge">
+//         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+//         <title>LinkStore</title>
+//         <link rel="stylesheet" href="payment.css">
+//         <style>
+//         container {
+//           background: #E3E6F3;
+//         }
+//         </style>
+//     </head>
+//     <body>
+//             <div class="container">
+//                 <div class="left">
+//                     <h3>BILLING ADDRESS</h3>
+                    
+//                     <form action="">
+//                         Full Name
+//                         <input type="text" name="" placeholder="Enter Name">
+//                         Email
+//                         <input type="text" name="" placeholder="Enter E-mail">
+//                         Address
+//                         <input type="text" name="" placeholder="Enter address">
+//                         City
+//                         <input type="text" name="" placeholder="Enter City">
+//                         <div id="zip">
+//                             <label>
+//                                 District
+//                                 <select name="" id="">
+//                                     <option value="null">Choose District</option>
+//                                     <option value="Rajasthan">Mumbai Suburban</option>
+//                                     <option value="Haryana">Mumbai City</option>
+//                                 </select>
+//                             </label>
+//                             <label for="">Zip Code
+//                             <input type="number" name="" id="" placeholder="Zip Code">
+//                             </label>
+//                         </div>
+//                     </form>
+//                 </div>
+//                 <div class="right">
+//                     <h3>PAYMENT</h3>
+//                     <form action="razor.html">
+//                         <div id="amountToPay">
+
+//                         </div>
+//                         <br>
+//                         <br>
+//                         Credit Card Number
+//                         <input type="text" name="" placeholder="Enter Credit Card Number">
+//                         <!-- Expiry Month
+//                         <input type="text" name="" placeholder="Enter City">
+//                         Expiry Year
+//                         <input type="text" name="" placeholder="Enter City"> -->
+//                         <div id="zip">
+//                             <label>
+//                                 Expiry Month
+//                                 <br>
+//                                 <select name="" id="">
+//                                     <option value="null">Choose Month</option>
+//                                     <option value="Rajasthan">2024</option>
+//                                     <option value="Haryana">2025</option>
+//                                     <option value="Haryana">2026</option>
+//                                 </select>
+//                             </label>
+//                             <label for="">
+//                                 Expiry Year
+//                                 <br>
+//                                 <select name="" id="">
+//                                     <option value="null">Choose Year</option>
+//                                     <option value="Rajasthan">2024</option>
+//                                     <option value="Haryana">2025</option>
+//                                     <option value="Haryana">2026</option>
+//                                 </select>       
+//                             </label>
+//                             <br>    
+//                         </div>
+    
+//                         <div id="Amount">
+//                             <label for="">Enter CVV
+//                                 <input type="number" name="" id="" placeholder="3 digit number">
+//                                 </label>
+//                             <label for="">Enter Amount
+//                                 <input type="number" name="" id="" placeholder="Enter Amount">
+//                                 </label>
+//                         </div>
+                        
+//                     </form>
+//                     <a href="razor.html"><button>Proceed</button></a>
+//                 </div>
+//             </div>
+        
+//     </body>
+//     </html>
+//     `;
+
+//     changeBody2.innerHTML = ``;
+//     changeBody3.innerHTML = ``;
+//     changeBody4.innerHTML = ``;
+    
+//     // var amountToPay = document.getElementById("amountToPay");
+//     // amountToPay.innerText = finalMoney;
+//     }
+    
+//   })
+// })();
+
+
 const payment = document.getElementById("payment");
 var changeBody1 = document.getElementById("product1");
 var changeBody2 = document.getElementById("product2");
@@ -144,15 +284,6 @@ var changeBody4 = document.getElementById("product4");
 // var payWall = document.getElementsByTagName("section");
 //console.log(changeBody1);
 
-
-(function() {
-  payment.addEventListener("click", function() {
-    
-  })
-})();
-
-
-
 (function() {
   payment.addEventListener("click", function() {
     if(cart_number == 0)
@@ -161,7 +292,6 @@ var changeBody4 = document.getElementById("product4");
     }
     else
     {
-      
       changeBody1.innerHTML = `
     <!DOCTYPE html>
     <html lang="en">
@@ -172,8 +302,9 @@ var changeBody4 = document.getElementById("product4");
         <title>LinkStore</title>
         <link rel="stylesheet" href="payment.css">
         <style>
-        container {
+        .container {
           background: #E3E6F3;
+          width:450px;
         }
         </style>
     </head>
@@ -181,16 +312,17 @@ var changeBody4 = document.getElementById("product4");
             <div class="container">
                 <div class="left">
                     <h3>BILLING ADDRESS</h3>
-                    
-                    <form action="">
+                    <form action="razor.html">
                         Full Name
-                        <input type="text" name="" placeholder="Enter Name">
+                        <input type="text" name="" placeholder="Enter Name" required>
                         Email
-                        <input type="text" name="" placeholder="Enter E-mail">
+                        <br>
+                        <input style="width:400px; height:35px; margin-bottom:5px;" type="email" name="" placeholder="Enter E-mail" required>
+                        <br>
                         Address
-                        <input type="text" name="" placeholder="Enter address">
+                        <input type="text" name="" placeholder="Enter address" required>
                         City
-                        <input type="text" name="" placeholder="Enter City">
+                        <input type="text" name="" placeholder="Enter City" required>
                         <div id="zip">
                             <label>
                                 District
@@ -201,60 +333,14 @@ var changeBody4 = document.getElementById("product4");
                                 </select>
                             </label>
                             <label for="">Zip Code
-                            <input type="number" name="" id="" placeholder="Zip Code">
+                            <input type="number" name="" id="" placeholder="Zip Code" required>
                             </label>
                         </div>
+                        <a href="razor.html"><input style="text-align: center; margin-top: 5px; width:250px;" type="submit" value="Proceed to Payment"></a>
                     </form>
+                    
                 </div>
-                <div class="right">
-                    <h3>PAYMENT</h3>
-                    <form action="">
-                        <div id="amountToPay">
-
-                        </div>
-                        <br>
-                        <br>
-                        Credit Card Number
-                        <input type="text" name="" placeholder="Enter Credit Card Number">
-                        <!-- Expiry Month
-                        <input type="text" name="" placeholder="Enter City">
-                        Expiry Year
-                        <input type="text" name="" placeholder="Enter City"> -->
-                        <div id="zip">
-                            <label>
-                                Expiry Month
-                                <br>
-                                <select name="" id="">
-                                    <option value="null">Choose Month</option>
-                                    <option value="Rajasthan">2024</option>
-                                    <option value="Haryana">2025</option>
-                                    <option value="Haryana">2026</option>
-                                </select>
-                            </label>
-                            <label for="">
-                                Expiry Year
-                                <br>
-                                <select name="" id="">
-                                    <option value="null">Choose Year</option>
-                                    <option value="Rajasthan">2024</option>
-                                    <option value="Haryana">2025</option>
-                                    <option value="Haryana">2026</option>
-                                </select>       
-                            </label>
-                            <br>    
-                        </div>
-    
-                        <div id="Amount">
-                            <label for="">Enter CVV
-                                <input type="number" name="" id="" placeholder="3 digit number">
-                                </label>
-                            <label for="">Enter Amount
-                                <input type="number" name="" id="" placeholder="Enter Amount">
-                                </label>
-                        </div>
-                        
-                    </form>
-                    <a href="razor.html"><button>Proceed</button></a>
+                    
                 </div>
             </div>
         
